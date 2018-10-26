28ddb9fae9d077f4ae8b8c7abe4e0451b7e5501f
Steven Anderson <steven@sjanderson.org>
2018-10-26 15:57:28 -0700

    trying again
    Signed-off-by: Steven Anderson <steven@sjanderson.org>

--- bin/dobi.mjs
+++ bin/dobi.mjs
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-':'// # comment; exec /usr/bin/env node --experimental-modules "$0" "$@"
+":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"
 
 import '../lib/cli';
