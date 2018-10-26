20d525f7108697aa349c69d60cdd1906256b6be4
Steven Anderson <steven@sjanderson.org>
2018-10-26 15:54:30 -0700

    try to get the damn bin to work
    Signed-off-by: Steven Anderson <steven@sjanderson.org>

---
+++ bin/dobi.mjs
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+':'// # comment; exec /usr/bin/env node --experimental-modules "$0" "$@"
+
+import '../lib/cli';
--- bin/dobi.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-BASEDIR=$( dirname "$0" )
-node --experimental-modules $BASEDIR/../lib/cli "$@"
